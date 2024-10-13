e0f68145c70c6a22a62d45e073eda95b5c471c36
Alen Alex <alengeoalex@gmail.com>
2024-10-13 19:03:05 +0530

    Fixed URL and Subject

--- MyServe.Backend.App.Common/Constants/EmailTemplate/EmailTemplate.cs
+++ MyServe.Backend.App.Common/Constants/EmailTemplate/EmailTemplate.cs
@@ -33,8 +33,8 @@ public class EmailTemplate(string templateKey, string subject)
         foreach (var (placeholder, replacement) in subjectPlaceholder)
         {
             var tempPlaceholderKey = placeholder;
-            if(!tempPlaceholderKey.StartsWith("{{"))
-                tempPlaceholderKey = "{{" + placeholder + "}}";
+            if(!tempPlaceholderKey.StartsWith("{("))
+                tempPlaceholderKey = "{(" + placeholder + ")}";
             
             tempSubject = tempSubject.Replace(tempPlaceholderKey, replacement);
         }
